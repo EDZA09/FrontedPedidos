d0216d5874464687ca829b4eb0a801d13b3ca7fd
EDZA09 <eduarstiven2001@gmail.com>
2021-11-24 13:11:10 -0500

    creando rutas a componentes y modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  },
+  {
+    //relacionando ruta "/" con la ruta "inicio"
+    path: '/',
+    pathMatch: 'full',
+    redirectTo: '/inicio',
+  },
+  {
+    //Implementando la estrategia Lazy Loading
+
+    //lazy loading del modulo de seguridad
+    path: 'seguridad',
+    loadChildren: () =>
+      import('./modulos/seguridad/seguridad.module').then(
+        (x) => x.SeguridadModule
+      ),
+  },
+  {
+    //lazy loading del modulo de administracion
+    path: 'administracion',
+    loadChildren: () =>
+      import('./modulos/administracion/administracion.module').then(
+        (x) => x.AdministracionModule
+      ),
+  },
+  {
+    //lazy loading del modulo de pedidos
+    path: 'pedidos',
+    loadChildren: () =>
+      import('./modulos/pedidos/pedidos.module').then((x) => x.PedidosModule),
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,10 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarPersonaComponent } from './persona/buscar-persona/buscar-persona.component';
+import { CrearPersonaComponent } from './persona/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './persona/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './persona/eliminar-persona/eliminar-persona.component';
+import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { EditarProductoComponent } from './productos/editar-producto/editar-producto.component';
+import { EliminarProductoComponent } from './productos/eliminar-producto/eliminar-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-persona',
+    component: CrearPersonaComponent,
+  },
+  {
+    path: 'editar-persona',
+    component: EditarPersonaComponent,
+  },
+  {
+    path: 'eliminar-persona',
+    component: EliminarPersonaComponent,
+  },
+  {
+    path: 'buscar-persona',
+    component: BuscarPersonaComponent,
+  },
+  {
+    path: 'crear-producto',
+    component: CrearProductoComponent,
+  },
+  {
+    path: 'buscar-producto',
+    component: BuscarProductoComponent,
+  },
+  {
+    path: 'editar-producto',
+    component: EditarProductoComponent,
+  },
+  {
+    path: 'eliminar-producto',
+    component: EliminarProductoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdministracionRoutingModule { }
+export class AdministracionRoutingModule {}
